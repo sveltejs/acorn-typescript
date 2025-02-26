ea386c4957f5d9294385056e97b7e2cdcd3c790f
Simon Holthausen <simon.holthausen@vercel.com>
2025-02-26 15:24:09 +0100

    fix: parse type assertions

--- index.d.ts
+++ index.d.ts
@@ -3,10 +3,13 @@ import { Parser } from 'acorn';
 declare function tsPlugin(options?: {
 	dts?: boolean;
 	allowSatisfies?: boolean;
-	jsx?: {
-		allowNamespaces?: boolean;
-		allowNamespacedObjects?: boolean;
-	};
+	/** Whether to use JSX. Defaults to false */
+	jsx?:
+		| boolean
+		| {
+				allowNamespaces?: boolean;
+				allowNamespacedObjects?: boolean;
+		  };
 }): (BaseParser: typeof Parser) => typeof Parser;
 
 export { tsPlugin, tsPlugin as default };
