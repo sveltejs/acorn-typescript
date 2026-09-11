1f1fe8c5df7c45da099c19db141ad912421f20b1
jake champion <jake.champion@netlify.com>
2026-09-11 16:55:42 +0100

    fix: allow non-null assertions in a decorator expression

--- src/extentions/decorators.ts
+++ src/extentions/decorators.ts
@@ -68,14 +68,29 @@ export default function generateParseDecorators(
 			// chain may follow either form: `@(foo).bar` as much as `@foo.bar`. Running this
 			// before the arguments below is what keeps `@foo().bar` rejected, since the
 			// grammar does not let a DecoratorCallExpression be extended.
-			while (this.eat(tt.dot)) {
-				const node = this.startNodeAt(startPos, startLoc);
-				node.object = expr;
-				// A decorator may also name a private class member, as in `@A.#dec`.
-				node.property =
-					this.type === tt.privateId ? this.parsePrivateIdent() : this.parseIdent(true);
-				node.computed = false;
-				expr = this.finishNode(node, 'MemberExpression');
+			for (;;) {
+				if (this.eat(tt.dot)) {
+					const node = this.startNodeAt(startPos, startLoc);
+					node.object = expr;
+					// A decorator may also name a private class member, as in `@A.#dec`.
+					node.property =
+						this.type === tt.privateId ? this.parsePrivateIdent() : this.parseIdent(true);
+					node.computed = false;
+					expr = this.finishNode(node, 'MemberExpression');
+				} else if (
+					// TypeScript lets a non-null assertion punctuate the chain: `@x!`, `@x!.y`.
+					!this.hasPrecedingLineBreak() &&
+					this.value === '!' &&
+					this.match(tt.prefix)
+				) {
+					this.exprAllowed = false;
+					this.next();
+					const node = this.startNodeAt(startPos, startLoc);
+					node.expression = expr;
+					expr = this.finishNode(node, 'TSNonNullExpression');
+				} else {
+					break;
+				}
 			}
 
 			node.expression = this.parseMaybeDecoratorArguments(expr);
